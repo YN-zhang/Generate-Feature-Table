f408f2719594fa5dc493b6a9696cd6c85f6231be
YN-zhang <36425408+YN-zhang@users.noreply.github.com>
2022-08-10 14:31:16 -0400

    Add files via upload

---
+++ trans_motif_tf.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import sys
+import numpy as np
+
+df = pd.read_csv('motif_tf.tsv',index_col=0,sep='\t')
+
+dict1=df.to_dict()
+dict2=dict1['Transcription factor']
+
+df2=pd.read_csv(sys.argv[1],index_col=0)
+res=pd.DataFrame()
+res['name']=df2.index
+
+
+for i in df2.columns:
+	res[dict2[i]]=np.zeros(len(df2.index)).astype(int)
+
+for i in df2.columns:
+	res[dict2[i]]=np.array(res[dict2[i]])|np.array(df2[i])
+res=res.set_index('name')
+temp_col=sorted(res.columns)
+print(temp_col)
+#print(res)
+#print(res.describe().T)
+res=pd.DataFrame(res,columns=temp_col)
+print(res.columns)
+res.to_csv(sys.argv[1]+'tf.csv')
